79ed14a931fcd1316cb860fe07135f665070bb00
Tia Self <tiaself2002@gmail.com>
2023-02-08 21:11:20 -0500

    Update Main Nick Hively <nrhively@bsu.edu>

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -1,13 +1,15 @@
 package edu.bsu.cs222;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String searchRequest;
 
-        Scanner scanner = new Scanner(System.in); //name the scanner something more specific to follow cc
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your search:");
         searchRequest = scanner.nextLine();
 
@@ -15,8 +17,15 @@ public class Main {
         user.userSearchRequest(searchRequest);
 
         URL url = new URL();
-        String encodedurl = url.createURL(searchRequest);
-        InputStream inputStream = url.connectURL(encodedurl);
+        String encodedUserURL = url.createURL(searchRequest);
+        InputStream userInputStream = url.connectURL(encodedUserURL);
+        String encodedTimeStampURL = url.createURL(searchRequest);
+        InputStream timeStampInputStream = url.connectURL(encodedTimeStampURL);
+
+        revisionFormatter formatter = new revisionFormatter();
+        ArrayList<String> userList = formatter.parseUser(userInputStream);
+        ArrayList<String> timestampList = formatter.parseTimeStamp(timeStampInputStream);
+        System.out.println(formatter.formatRevisions(userList,timestampList));
 
     }
 }
--- src/main/java/edu/bsu/cs222/revisionFormatter.java
+++ src/main/java/edu/bsu/cs222/revisionFormatter.java
@@ -7,7 +7,7 @@ public class revisionFormatter extends revisionParser{
     public ArrayList<String> formatRevisions (ArrayList<String> parsedUsersList, ArrayList<String> parsedTimeStampList){
         ArrayList<String> revisionsList = new ArrayList<>();
         for (int item = 0; item < parsedUsersList.size(); item++) {
-            revisionsList.add(parsedTimeStampList.get(item) + " " + parsedUsersList.get(item));
+            revisionsList.add(parsedTimeStampList.get(item) + " " + parsedUsersList.get(item)+"\n");
         }return revisionsList;
     }
 }
