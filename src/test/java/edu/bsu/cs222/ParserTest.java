b726cb17d39623b947b158ec4e0928e796b15dd5
nickrh11 <nrhively@bsu.edu>
2023-02-20 18:56:58 -0500

    Add missingPageTest in ParserTest class Nick Hively <nrhively@bsu.edu>

--- src/test/java/edu/bsu/cs222/ParserTest.java
+++ src/test/java/edu/bsu/cs222/ParserTest.java
@@ -10,13 +10,15 @@ import java.util.ArrayList;
 
 public class ParserTest {
 
-    private final InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
-    private final DocumentContext jsonContext = JsonPath.parse(inputStream);
+    private final InputStream inputTestStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+    private final InputStream missingPage = Thread.currentThread().getContextClassLoader().getResourceAsStream("missingPage.json");
+    private final DocumentContext jsonContext = JsonPath.parse(inputTestStream);
+    private final DocumentContext missingPageJSON = JsonPath.parse(missingPage);
+    Parser parser = new Parser();
 
     @Test
     public void parseForUsersTest() {
 
-        Parser parser = new Parser();
         ArrayList<String> output = parser.parseUser(jsonContext);
         Assertions.assertEquals("Jpgordon", output.get(0));
 
@@ -25,7 +27,6 @@ public class ParserTest {
     @Test
     public void parseForTimeStampTest() {
 
-        Parser parser = new Parser();
         ArrayList<String> output = parser.parseTimeStamp(jsonContext);
         Assertions.assertEquals("2023-01-07T16:32:37Z", output.get(0));
 
@@ -34,7 +35,6 @@ public class ParserTest {
     @Test
     public void parseForRedirectTest() {
 
-        Parser parser = new Parser();
         ArrayList<String> output = parser.parseRedirectTo(jsonContext);
         Assertions.assertEquals("Frank Zappa", output.get(0));
 
@@ -43,7 +43,6 @@ public class ParserTest {
     @Test
     public void parseRevisionListUserTest() {
 
-        Parser parser = new Parser();
         ArrayList<Revision> revisionArrayList = parser.parse(jsonContext);
         Assertions.assertEquals("Jpgordon", revisionArrayList.get(0).user);
 
@@ -52,7 +51,6 @@ public class ParserTest {
     @Test
     public void parseRevisionListTimeStampTest() {
 
-        Parser parser = new Parser();
         ArrayList<Revision> revisionArrayList = parser.parse(jsonContext);
         Assertions.assertEquals("2023-01-05T13:00:13Z", revisionArrayList.get(3).timestamp);
 
@@ -61,11 +59,19 @@ public class ParserTest {
     @Test
     public void parseRevisionListLengthTest() {
 
-        Parser parser = new Parser();
         ArrayList<Revision> revisionArrayList = parser.parse(jsonContext);
         Assertions.assertEquals(4, revisionArrayList.size());
 
     }
 
+    @Test
+    public void missingPageTest(){
+        try {
+            parser.parsePageMissing(missingPageJSON);
+        } catch (Error error){
+            assert(error).getMessage().equals("Page does not exist");
+        }
+    }
 }
+
 
