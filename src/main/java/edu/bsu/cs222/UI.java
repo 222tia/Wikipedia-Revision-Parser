c7351aa5ae2ff965e14934ab89e34600c61ad828
nickrh11 <nrhively@bsu.edu>
2023-02-20 19:09:10 -0500

    Refactor UI class to follow single responsibility principle Nick Hively <nrhively@bsu.edu>

--- src/main/java/edu/bsu/cs222/UI.java
+++ src/main/java/edu/bsu/cs222/UI.java
@@ -16,7 +16,9 @@ import java.io.ByteArrayInputStream;
 
 
 public class UI extends Application {
-    public static void main(String[] args){ launch(args); }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     private final Button searchButton = new Button("Search");
     private final Label searchLabel = new Label("Enter your search:");
@@ -29,7 +31,7 @@ public class UI extends Application {
         configureInterface(primaryStage);
     }
 
-    private void configureInterface(Stage stage){
+    private void configureInterface(Stage stage) {
         stage.setTitle("Wikipedia Revision Parser");
         stage.setScene(new Scene(userInterface()));
         stage.show();
@@ -37,7 +39,7 @@ public class UI extends Application {
 
     private Pane userInterface() {
         VBox interfaceVBox = new VBox();
-        interfaceVBox.setPrefSize(400,500);
+        interfaceVBox.setPrefSize(400, 500);
 
         configureLabelStyle();
         configureOutputField();
@@ -50,48 +52,49 @@ public class UI extends Application {
         return interfaceVBox;
     }
 
-    private void configureLabelStyle(){
+    private void configureLabelStyle() {
         searchLabel.setFont(Font.font("Calibri", 15));
         outputLabel.setFont(Font.font("Calibri", 15));
     }
 
-    private void configureOutputField(){
+    private void configureOutputField() {
         outputField.setPrefSize(400, 400);
         outputField.setEditable(false);
         outputField.setWrapText(true);
     }
 
-    protected String noLineFoundCheck(){
-        if (searchField.getText().isEmpty()){
+    protected String noLineFoundCheck() {
+        if (searchField.getText().isEmpty()) {
             throw new Error("User did not provide an input");
-        } return searchField.getText();
+        }
+        return searchField.getText();
     }
 
     private void configureSearchButton() {
         searchButton.setFont(Font.font("Calibri", 15));
+        searchButton.setOnAction(event -> searchForPage());
 
+    }
+
+    private void searchForPage() {
         Formatter formatter = new Formatter();
-
-        searchButton.setOnAction(event -> {
-            try {
-                String userSearch = noLineFoundCheck();
-                System.setIn(new ByteArrayInputStream(userSearch.getBytes()));
-                DocumentContext jsonContext = formatter.createJSONContext();
-                formatter.parsePageMissing(jsonContext);
-                String formattedStringList = formatter.revisionsToStringFormatter(jsonContext);
-                outputField.setText("");
-                if (formatter.checkIfRedirect(jsonContext)) {
-                    outputField.appendText(formatter.formatRedirect(jsonContext));
-                }
-                outputField.appendText(formattedStringList);
-            } catch (RuntimeException ioException) {
-                outputField.setText("Network connection problem" + ioException.getMessage());
-            } catch (Error error) {
-                outputField.setText(error.getMessage());
+        try {
+            String userSearch = noLineFoundCheck();
+            System.setIn(new ByteArrayInputStream(userSearch.getBytes()));
+            DocumentContext jsonContext = formatter.createJSONContext();
+            formatter.parsePageMissing(jsonContext);
+            String formattedStringList = formatter.revisionsToStringFormatter(jsonContext);
+            outputField.setText("");
+            if (formatter.checkIfRedirect(jsonContext)) {
+                outputField.appendText(formatter.formatRedirect(jsonContext));
+                outputField.appendText("\n");
             }
-        });
-
-
+            outputField.appendText(formattedStringList);
+        } catch (RuntimeException ioException) {
+            outputField.setText("Network connection problem" + ioException.getMessage());
+        } catch (Error error) {
+            outputField.setText(error.getMessage());
+        }
     }
 }
 
